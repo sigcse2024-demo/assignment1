e4a79211532832c4709e25192449b207ed986951
heymrhayes <heymrhayes@users.noreply.github.com>
2024-03-23 00:48:57 +0000

    Mar 23, 2024, 12:48 AM

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
 for i in range(10,2000,33):
     print(i)
     
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+nums = [3,4,5]
+
+# some comment
+#   some more comments
+#     even more comments
